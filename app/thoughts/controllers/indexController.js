b210dff3911f0663d858dfded9722b8d7abe9cdd
Anthony Nardi <anardi@aisquared.com>
2014-11-12 08:41:01 -0500

    Updating colors, using less

---
+++ app/thoughts/controllers/indexController.js
@@ -0,0 +1,50 @@
+var fs   = require('fs'),
+    path = require('path'),
+    jade = require('jade');
+
+var controller = {
+
+  'index': function (request, response, logger) {
+
+    var template = path.normalize(__dirname + '/../templates/index.jade');
+    var includes = path.normalize('base/templates/includes.jade');
+
+
+    fs.exists(template, function (exists) {
+
+      if (exists) {
+
+        fs.readFile(template, function (err, data) {
+
+          if (err) {
+            response.write('Error');
+            console.log(err);
+            response.end();
+          }
+
+          var fn = jade.compile(data, {
+            'filename': includes
+          });
+
+          response.write(fn({}));
+          response.end();
+
+        });
+
+      } else {
+
+        console.log(template + ' does not exist')
+
+      }
+
+    });
+
+  },
+
+  'login': function () {
+    console.log('login');
+  }
+
+};
+
+module.exports = controller;
