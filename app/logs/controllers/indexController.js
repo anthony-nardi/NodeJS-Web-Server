bbeebb155786bb649c636ce84a561aa00d168516
Anthony Nardi <anardi@aisquared.com>
2014-11-13 15:26:26 -0500

    Logs!

--- app/logs/controllers/indexController.js
+++ app/logs/controllers/indexController.js
@@ -2,36 +2,64 @@
 
 var fs   = require('fs'),
     path = require('path'),
+    jade = require('jade'),
     pg   = require('pg');
 
 var controller = {
 
-  'index': function (request, response) {
+  'index': function (request, response, logger) {
+
+    var template = path.normalize(__dirname + '/../templates/index.jade');
+    var includes = path.normalize('base/templates/includes.jade');
 
     pg.connect('postgres://anardi:password@localhost', function(err, client) {
 
       if (err) console.log('ERROR CONNECTING TO POSTGRES', err);
 
-      client.query('SELECT * FROM log', function(err, result) {
+      client.query('select orig_message, nice_message, date, modulename from log left outer join modules on (log.moduleid = modules.moduleid)', function(err, result) {
 
         if(err) {
          return console.error('error running query', err);
         }
 
-        console.log(result.rows);
+        fs.exists(template, function (exists) {
 
-        response.end(JSON.stringify(result.rows));
+          if (exists) {
 
-        //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+            fs.readFile(template, function (err, data) {
+
+              if (err) {
+                response.write('Error');
+                console.log(err);
+                response.end();
+              }
+
+              var fn = jade.compile(data, {
+                'filename': includes
+              });
+
+              response.write(fn({
+                'logs': result.rows
+              }));
+
+              response.end();
+
+            });
+
+          } else {
+
+            console.log(template + ' does not exist')
+
+          }
+
+        });
 
         client.end();
 
       });
 
     });
-
   }
-
 };
 
 module.exports = controller;
--- app/logs/skin/js/general.js
+++ app/logs/skin/js/general.js
@@ -1,4 +1,73 @@
+'use strict';
 
-(function () {
-  console.log('THE INDEX JS FILE');
-}())
+document.addEventListener('DOMContentLoaded', function () {
+
+  if (!window.logs) {
+    console.log(window.logs);
+  } else {
+    console.log('Logs found: %o', window.logs);
+
+    var parentElement = document.getElementById('content'),
+        logs = window.logs,
+        logCount = logs.length,
+        i = 0,
+        moduleName,
+        date,
+        niceMessage,
+        origMessage,
+        log;
+
+    // For every log:
+    //  1) Create a row
+    //  2) Create a column for every property of each log
+    for (; i < logCount; i++) {
+      log = logs[i];
+      moduleName = log.modulename;
+      date = log.date;
+      niceMessage = log.nice_message;
+      origMessage = log.orig_message;
+      parentElement.appendChild(getLogHTML(moduleName, niceMessage, origMessage, date));
+    }
+
+
+  }
+
+});
+
+function createDiv () {
+  return document.createElement('div');
+}
+
+function createTextNode (string) {
+  return document.createTextNode(string);
+}
+
+function getLogHTML (moduleName, niceMessage, origMessage, date) {
+
+  var rowDiv               = createDiv(),
+      moduleNameColumnDiv  = createDiv(),
+      niceMessageColumnDiv = createDiv(),
+      origMessageColumnDiv = createDiv(),
+      dateColumnDiv        = createDiv(),
+      moduleNameTextNode   = createTextNode(moduleName),
+      niceMessageTextNode  = createTextNode(niceMessage),
+      origMessageTextNode  = createTextNode(origMessage),
+      dateTextNode         = createTextNode(Date(date));
+
+  rowDiv.className               = 'row';
+  moduleNameColumnDiv.className  = 'col-md-2 col-xs-2';
+  niceMessageColumnDiv.className = 'col-md-8 col-xs-8';
+  dateColumnDiv.className        = 'col-md-2 col-xs-2';
+
+  moduleNameColumnDiv.appendChild(moduleNameTextNode);
+  niceMessageColumnDiv.appendChild(niceMessageTextNode);
+  // origMessageColumnDiv.appendChild(origMessageTextNode);
+  dateColumnDiv.appendChild(dateTextNode);
+
+  rowDiv.appendChild(moduleNameColumnDiv);
+  rowDiv.appendChild(niceMessageColumnDiv);
+  rowDiv.appendChild(dateColumnDiv);
+
+  return rowDiv;
+
+}
