7ca8ddb315e7e52645bc6a245d105d0479184652
Anthony Nardi <anardi@aisquared.com>
2014-11-13 12:48:37 -0500

    Adding more tables, must refactor soon!

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var PORT    = 5000,
 
     app    	= http.Server(requestListener),
 
-    loggingClient;
+    loggingClient,
+    moduleMap = {};
 
 
 
@@ -31,9 +32,10 @@ pg.connect('postgres://anardi:password@localhost', function(err, client) {
 
 });
 
-function logger (moduleId, orig_message, nice_message) {
+function logger (moduleName, orig_message, nice_message) {
 
   var table = 'log',
+      moduleId = moduleMap[moduleName],
       query;
 
   if (!loggingClient) {
@@ -53,7 +55,7 @@ function logger (moduleId, orig_message, nice_message) {
                           Date.now() + '\')';
 
     loggingClient.query(query, function (err, result) {
-      if (err) console.log(err);
+      if (err)    console.log(err);
       if (result) console.log(result);
     });
 
@@ -64,7 +66,7 @@ function logger (moduleId, orig_message, nice_message) {
 function createLogTable () {
   var query = 'CREATE TABLE IF NOT EXISTS log ( moduleId integer NOT NULL, orig_message text, nice_message text, date integer )';
   loggingClient.query(query, function (err, result) {
-    if (err) console.log('ERR: ', err);
+    if (err)    console.log('ERR: ', err);
     if (result) console.log('RES: ', result);
   });
 }
@@ -72,9 +74,71 @@ function createLogTable () {
 function createModulesTable () {
   var query = 'CREATE TABLE IF NOT EXISTS modules ( moduleId integer NOT NULL, moduleName varchar(40) NOT NULL )';
   loggingClient.query(query, function (err, result) {
-    if (err) console.log('ERR: ', err);
-    if (result) console.log('RES: ', result);
+    if (err) {
+      console.log('ERR: ', err);
+    }
+    if (result) {
+      console.log('RES: ', result);
+      populateModulesTable();
+    }
   });
+}
+
+function populateModulesTable () {
+  var selectQuery = 'SELECT * FROM modules',
+      insertQuery = 'INSERT INTO modules VALUES ' +
+                    '(0, \'core\'), ' +
+                    '(1, \'about\'), ' +
+                    '(2, \'admin\'), ' +
+                    '(3, \'audio\'), ' +
+                    '(4, \'blog\'), ' +
+                    '(5, \'demos\'), ' +
+                    '(6, \'index\'), ' +
+                    '(7, \'logs\'), ' +
+                    '(8, \'music\'), ' +
+                    '(9, \'quad\'), ' +
+                    '(10, \'sms\'), ' +
+                    '(11, \'thoughts\')';
+  loggingClient.query(selectQuery, function (err, result) {
+    if (err) {
+      console.log('ERR: ', err);
+      return;
+    }
+    if (!result.rows || !result.rows.length) {
+      loggingClient.query(insertQuery, function (err, result) {
+        if (err) {
+          console.log('ERR: ', err);
+          return;
+        }
+        console.log('RES: ', result);
+        setModuleMap();
+      })
+    } else {
+      setModuleMap(result);
+    }
+  });
+}
+
+function setModuleMap (result) {
+  var selectQuery = 'SELECT * FROM modules';
+
+  if (result) {
+    for (var i = 0; i < result.rows.length; i += 1) {
+      moduleMap[result.rows[i].modulename] = result.rows[i].moduleid;
+    }
+    console.log(moduleMap);
+  } else {
+    loggingClient.query(selectQuery, function (err, result) {
+      if (err) {
+        console.log('ERR: ', err);
+        return;
+      }
+      for (var i = 0; i < result.rows.length; i += 1) {
+        moduleMap[result.rows[i].modulename] = result.rows[i].moduleid;
+      }
+      console.log(moduleMap);
+    });
+  }
 }
 
 function createTables () {
