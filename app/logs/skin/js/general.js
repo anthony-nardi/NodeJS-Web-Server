ad130508fb7b85fcd0c54a6dd569bfa47e0ef495
Anthony Nardi <anardi@aisquared.com>
2014-11-14 11:00:45 -0500

    Improving frontend logging functionality

--- app/logs/skin/js/general.js
+++ app/logs/skin/js/general.js
@@ -10,12 +10,13 @@ document.addEventListener('DOMContentLoaded', function () {
     var parentElement = document.getElementById('content'),
         logs = window.logs,
         logCount = logs.length,
-        i = 0,
+        i = 0, k = 0,
         moduleName,
         date,
         niceMessage,
         origMessage,
-        log;
+        log,
+        logRows;
 
     // For every log:
     //  1) Create a row
@@ -27,6 +28,12 @@ document.addEventListener('DOMContentLoaded', function () {
       niceMessage = log.nice_message;
       origMessage = log.orig_message;
       parentElement.appendChild(getLogHTML(moduleName, niceMessage, origMessage, date));
+    }
+
+    logRows = document.getElementsByClassName('logRow');
+
+    for (; k < logRows.length; k++) {
+      logRows[k].addEventListener('click', toggleMessage);
     }
 
 
@@ -54,20 +61,38 @@ function getLogHTML (moduleName, niceMessage, origMessage, date) {
       origMessageTextNode  = createTextNode(origMessage),
       dateTextNode         = createTextNode(Date(date));
 
-  rowDiv.className               = 'row';
+  rowDiv.className               = 'row logRow';
   moduleNameColumnDiv.className  = 'col-md-2 col-xs-2';
-  niceMessageColumnDiv.className = 'col-md-8 col-xs-8';
+  niceMessageColumnDiv.className = 'col-md-8 col-xs-8 niceMessage';
+  origMessageColumnDiv.className = 'col-md-8 col-xs-8 hidden origMessage';
   dateColumnDiv.className        = 'col-md-2 col-xs-2';
 
   moduleNameColumnDiv.appendChild(moduleNameTextNode);
   niceMessageColumnDiv.appendChild(niceMessageTextNode);
-  // origMessageColumnDiv.appendChild(origMessageTextNode);
+  origMessageColumnDiv.appendChild(origMessageTextNode);
   dateColumnDiv.appendChild(dateTextNode);
 
   rowDiv.appendChild(moduleNameColumnDiv);
   rowDiv.appendChild(niceMessageColumnDiv);
+  rowDiv.appendChild(origMessageColumnDiv);
   rowDiv.appendChild(dateColumnDiv);
 
   return rowDiv;
+
+}
+
+function toggleMessage (event) {
+
+  var rowClicked = event.currentTarget,
+      niceMessage = rowClicked.getElementsByClassName('niceMessage')[0],
+      origMessage = rowClicked.getElementsByClassName('origMessage')[0];
+
+  if (niceMessage.className.indexOf('hidden') === -1) {
+    niceMessage.className = niceMessage.className + ' hidden';
+    origMessage.className = origMessage.className.replace('hidden', '');
+  } else {
+    origMessage.className = origMessage.className + ' hidden';
+    niceMessage.className = niceMessage.className.replace('hidden', '');
+  }
 
 }
